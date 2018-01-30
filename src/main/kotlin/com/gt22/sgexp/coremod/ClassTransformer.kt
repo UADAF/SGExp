76acfa544c98169f72be27337278e823f2dcd4da
gt22 <igorengel@mail.ru>
2018-01-30 16:06:23 +0300

    ComputerInterface support for energy req with accelerator

--- src/main/kotlin/com/gt22/sgexp/coremod/ClassTransformer.kt
+++ src/main/kotlin/com/gt22/sgexp/coremod/ClassTransformer.kt
@@ -10,7 +10,11 @@ import net.minecraft.launchwrapper.IClassTransformer
 
 class ClassTransformer : IClassTransformer {
     override fun transform(name: String, transformedName: String, basicClass: ByteArray): ByteArray {
-        return if(name == "gcewing.sg.SGBaseTE") patchSGBase(name, basicClass) else basicClass
+        return if(name == "gcewing.sg.SGBaseTE")
+            patchSGBase(name, basicClass)
+        else if(name == "gcewing.sg.SGInterfaceTE")
+            patchSGInterface(name, basicClass)
+        else basicClass
     }
 
     private fun patchSGBase(name: String, data: ByteArray): ByteArray {
@@ -21,24 +25,35 @@ class ClassTransformer : IClassTransformer {
             for(i in it.instructions) {
                 if(i.opcode == Opcodes.BIPUSH) {
                     val load = VarInsnNode(Opcodes.ALOAD, 0)
-                    it.instructions.set(i, load)
-                    it.instructions.insert(load, MethodInsnNode(Opcodes.INVOKESTATIC, "com/gt22/sgexp/coremod/Methods", "getAcceleratedDialingTime", "(Lgcewing/sg/SGBaseTE;)I"))
+                    it.instructions.insert(i, load)
+                    it.instructions.insert(load, MethodInsnNode(Opcodes.INVOKESTATIC, "com/gt22/sgexp/coremod/Methods", "getAcceleratedDialingTime", "(ILgcewing/sg/SGBaseTE;)I"))
                     return@let
                 }
             }
         }
-        node.methods.find { it.name == "connect"}?.let(this::modifyEnergy)
-        node.methods.find { it.name == "finishDiallingAddress"}?.let(this::modifyEnergy)
-        node.methods.find { it.name == "tickEnergyUsage"}?.let(this::modifyEnergy)
+        node.methods.find { it.name == "connect"}?.to(0)?.let(this::modifyEnergy)
+        node.methods.find { it.name == "finishDiallingAddress"}?.to(0)?.let(this::modifyEnergy)
+        node.methods.find { it.name == "tickEnergyUsage"}?.to(0)?.let(this::modifyEnergy)
         val writer = ClassWriter(ClassWriter.COMPUTE_MAXS or ClassWriter.COMPUTE_FRAMES)
         node.accept(writer)
         return writer.toByteArray()
     }
 
-    private fun modifyEnergy(it: MethodNode) {
+    private fun patchSGInterface(name: String, data: ByteArray): ByteArray {
+        val node = ClassNode()
+        val reader = ClassReader(data)
+        reader.accept(node, 0)
+        node.methods.find { it.name == "ciEnergyToDial"}?.to(2)?.let(this::modifyEnergy)
+        val writer = ClassWriter(ClassWriter.COMPUTE_MAXS or ClassWriter.COMPUTE_FRAMES)
+        node.accept(writer)
+        return writer.toByteArray()
+    }
+
+    private fun modifyEnergy(m: Pair<MethodNode, Int>) {
+        val it = m.first
         for(i in it.instructions) {
             if(i.opcode == Opcodes.DMUL) {
-                val load = VarInsnNode(Opcodes.ALOAD, 0)
+                val load = VarInsnNode(Opcodes.ALOAD, m.second)
                 it.instructions.insert(i, load)
                 it.instructions.insert(load, MethodInsnNode(Opcodes.INVOKESTATIC, "com/gt22/sgexp/coremod/Methods", "getAcceleratedEnergyUse", "(DLgcewing/sg/SGBaseTE;)D"))
                 break
@@ -47,8 +62,8 @@ class ClassTransformer : IClassTransformer {
     }
 
     companion object {
-        fun getAcceleratedDialingTime(te: SGBaseTE): Int {
-            var time = 40
+        fun getAcceleratedDialingTime(defaultTime: Int, te: SGBaseTE): Int {
+            var time = defaultTime
             val t = te.localToGlobalTransformation()
             for (i in -2..2) {
                 val bp = t.p(i.toDouble(), -1.0, 0.0).blockPos()
--- src/main/kotlin/com/gt22/sgexp/coremod/Methods.java
+++ src/main/kotlin/com/gt22/sgexp/coremod/Methods.java
@@ -4,8 +4,8 @@ import gcewing.sg.SGBaseTE;
 
 public class Methods {
 
-	public static int getAcceleratedDialingTime(SGBaseTE te) {
-		return ClassTransformer.Companion.getAcceleratedDialingTime(te);
+	public static int getAcceleratedDialingTime(int defaultTime, SGBaseTE te) {
+		return ClassTransformer.Companion.getAcceleratedDialingTime(defaultTime, te);
 	}
 
 	public static double getAcceleratedEnergyUse(double energy, SGBaseTE te) {
